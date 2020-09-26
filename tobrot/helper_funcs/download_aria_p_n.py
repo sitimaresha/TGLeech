5142452124b246187e297aaa363fa1226f43a693
sitimaresha <71918419+sitimaresha@users.noreply.github.com>
2020-09-26 13:21:32 +0700

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -39,6 +39,7 @@ async def aria_start():
     aria2_daemon_start_cmd = []
     # start the daemon, aria2c command
     aria2_daemon_start_cmd.append("aria2c")
+    aria2_daemon_start_cmd.append("--conf-path=/app/aria2c.conf")
     aria2_daemon_start_cmd.append("--allow-overwrite=true")
     aria2_daemon_start_cmd.append("--daemon=true")
     # aria2_daemon_start_cmd.append(f"--dir={DOWNLOAD_LOCATION}")
