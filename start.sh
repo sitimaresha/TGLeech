c9799309c4a9b4ef7370f24ef353ea95ee821b2b
sitimaresha <71918419+sitimaresha@users.noreply.github.com>
2020-09-26 13:22:49 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+# Tracker
+bash <(curl -fsSL git.io/tracker.sh) "/app/aria2c.conf"
+
+#Peer Agent
+echo "peer-id-prefix=-qB4250-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!~*()._-' | fold -w 12 | head -n 1)" >> /app/aria2c.conf
+
 python3 -m tobrot
